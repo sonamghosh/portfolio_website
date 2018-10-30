79f97c1d45bbaa039bf0bcbf81e636558893dba4
Sonam Ghosh <sonamghosh@gmail.comu>
2018-10-30 15:14:02 -0400

    Added education and work information

--- src/resumeData.js
+++ src/resumeData.js
@@ -4,7 +4,7 @@ let resumeData = {
   "role": "Frontend Developer",
   "linkedinId":"sonamghosh96",
   "skypeid": "skypegear96",
-  "roleDescription": "Hello World, I am Software Developer that specializes in Artificial Intelligence and Quantum Computing. I dabble in work dealing numerical analysis, machine learning, deep learning, data science, agumented reality, computer vision, physics/chemistry modeling. I am also a avid world traveller and have been to almost 20 countries and try to travel internationally multiple times a year to experience new cultures, food, and experiences. TBC LOL Memes Camel",
+  "roleDescription": "Hello World, I am Software Developer that specializes in Artificial Intelligence and Quantum Computing. I dabble in work dealing numerical analysis, machine learning, deep learning, digital signal processing, data science, agumented reality, computer vision, physics/chemistry modeling. I am also a avid world traveller and have been to almost 20 countries and try to travel internationally multiple times a year to experience new cultures, food, and experiences. TBC LOL Memes Camel",
   "socialLinks":[
       {
         "name":"linkedin",
@@ -32,38 +32,55 @@ let resumeData = {
   "website":"https://www.codeclassifiers.com",
   "education":[
     {
-      "UniversityName":"Mumbai University",
-      "specialization":"Some specialization",
-      "MonthOfPassing":"Jan",
+      "UniversityName":"Boston University",
+      "specialization":"B.S. in Electrical Engineering and Minor in Physics",
+      "MonthOfPassing":"May",
       "YearOfPassing":"2018",
       "Achievements":"Some Achievements"
     },
     {
-      "UniversityName":"Some University",
-      "specialization":"Some specialization",
-      "MonthOfPassing":"Jan",
-      "YearOfPassing":"2018",
+      "UniversityName":"National University of Singapore",
+      "specialization":"Study Abroad Exchange Program Spring 2017 in Engineering",
+      "MonthOfPassing":"Jan 2017-May",
+      "YearOfPassing":"2017",
       "Achievements":"Some Achievements"
     }
   ],
   "work":[
     {
-      "CompanyName":"Some Company",
-      "specialization":"Some specialization",
-      "MonthOfLeaving":"Jan",
+      "CompanyName":"MITRE",
+      "specialization":"Machine Learning Signal Processing Engineer",
+      "MonthOfLeaving":"June 2018 - October",
       "YearOfLeaving":"2018",
-      "Achievements":"Some Achievements"
+      "Achievements":"Developed Python code to do data preprocessing, feature extraction, feature plotting, and classification of radio-frequency signals using signal processing and machine learning techniques. Utilized signal analysis techniques such as Empirical Mode Decomposition, Hilbert Analysis, and Fourier-Bessel expansion analysis. Utilized Machine Learning models like Random Forest Classifiers, Support Vector Machines, Naive-Bayes, and Multi-Layer perceptrons and t-Stochastic Neighbor Embedding for class visualization."
     },
     {
-      "CompanyName":"Some Company",
-      "specialization":"Some specialization",
-      "MonthOfLeaving":"Jan",
+      "CompanyName":"MITRE",
+      "specialization":"Signal Processing Engineer Intern",
+      "MonthOfLeaving":"May 2017 - May",
       "YearOfLeaving":"2018",
-      "Achievements":"Some Achievements"
+      "Achievements":"Developed Python classes and scripts related to fault generation in PNT (Position, Navigation, Timing) applications. Developed C++ Physics Gravity model for PNT application."
+    },
+    {
+      "CompanyName":"Quantum Communication \& Measurement Lab at Boston University",
+      "specialization":"Undergraduate Quantum Computing/Optics Researcher",
+      "MonthOfLeaving":"September 2017 - May",
+      "YearOfLeaving":"2018",
+      "Achievements":"Defended Senior Honors Thesis on demonstrating the potential of a linear-optical system to implement quantum simulations of a physical system such as a molecule. Constructed and simulated the Hamiltonian, calculated the eigenstates and eigenvalues, and photon quantum walks of a optical benzene topology of connected directionally unbiased optical multiports using Python and the QuTip quantum mechanics software package."
+    },
+    {
+      "CompanyName":"Ultrafast Optics Lab at Boston University",
+      "specialization":"Undergraduate Research Assistant",
+      "MonthOfLeaving":"September 2015 - April",
+      "YearOfLeaving":"2016",
+      "Achievements":"COnducted spectral analysis for custom built Photothermal Spectroscopy system. Learned the fundamentals of Photothermal Spectroscopy, Fourier-Transform Infrared Spectroscopy, and different types of Microscopy. Designed Microscopy stage and holder setup, wrote protocols for various microscope setups, and made a MATLAB script for counting number of cells/circular objects in a sample."
     }
   ],
   "skillsDescription":"Your skills here",
   "skills":[
+    {
+      "skillname":"Python"
+    },
     {
       "skillname":"HTML5"
     },
